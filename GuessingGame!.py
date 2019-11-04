c94332c0f67cb31d3a7fe6100b9b459521d70382
emmaputnam <57371623+emmaputnam@users.noreply.github.com>
2019-11-04 12:18:48 -0800

    Add files via upload

---
+++ GuessingGame!.py
@@ -0,0 +1,11 @@
+print("This is the guessing game, have fun!")
+
+print("This is how you play: I chose four different numbers in a specific order")
+print("You wil  guess a variety of four digit numbers unitl your guess is correct")
+print("I will tell you if your guess contains the right number(s)")
+print("Let's play!!!!")
+
+import random
+randonumber = raandom.randit (1000,999)
+print(randonumber)
+list1 = [int ]
